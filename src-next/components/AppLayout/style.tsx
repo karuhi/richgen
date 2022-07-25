daeb12d0d2f741e127c9d3056fc4f03f2b25fc46
かる <karuhi.dev@gmail.com>
2022-07-26 01:33:23 +0900

    feat: nextauriな環境を整えた。

---
+++ src-next/components/AppLayout/style.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: gray;
+  border-radius: 99999px;
+  border: 4px solid: black;
+`;
